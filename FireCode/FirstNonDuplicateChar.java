9c9e6616561299c1f9b96ccf0d22db4255bc367e
SasiDharKM <sasidhar.km97@gmail.com>
2019-10-08 21:08:59 -0400

    Added another solution

---
+++ FireCode/FirstNonDuplicateChar.java
@@ -0,0 +1,42 @@
+/* Find the first non-duplicate character in a string. 
+   Return null if no unique character is found.
+
+firstNonRepeatedCharacter( "abcdcd" ) --> 'a'
+firstNonRepeatedCharacter( "cbcd" ) --> 'b'
+firstNonRepeatedCharacter( "cdcd" ) --> null 
+
+*/
+import java.utils.*;
+import java.util.streams.*;
+
+// the improvement for correctness
+
+}
+
+
+
+public static Character firstNonRepeatedCharacter(String str) {
+    HashMap<Character, Integer> nonDup = new HashMap<>();
+    Character ans =null;
+    for (int i=0; i<str.length(); i++){
+
+        //increase count if already present in HashMap
+        if(nonDup.containsKey(str.charAt(i))){
+            int value = nonDup.get(str.charAt(i));
+            nonDup.put(str.charAt(i), value + 1);
+        }
+        else{
+            nonDup.put(str.charAt(i),1);
+        }
+    }
+    //return only characters with count of 1
+    //Problem is that the first one need not be the output 
+    //We could instead use an array[256] to maintain order instead of HashSet
+    for(Character s : nonDup.keySet()){
+        if(nonDup.get(s) == 1){
+            ans = s;
+            break;
+        }
+    }
+    return ans;
+}
