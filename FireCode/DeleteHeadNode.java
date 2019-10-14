e417341d4159cd4af625f348125be7a64f32b2dc
Sasidhar Kumar <sasidhar.km97@gmail.com>
2019-10-13 22:05:39 -0400

    Create DeleteHeadNode.java

---
+++ FireCode/DeleteHeadNode.java
@@ -0,0 +1,16 @@
+/*
+Given a singly-linked list, write a method to delete the first node of the list and return the new head.
+Example:
+
+LinkedList: 1->2->3 , Head = 1
+
+deleteAtHead(Head) ==> 2->3
+*/
+
+public ListNode deleteAtHead(ListNode head) {
+    if(head != null){
+        return head.next;
+        
+    }
+    return head;
+}
